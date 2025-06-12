7c9258c4213ecdf29bfca57b9be71d10af4ed463
Ch-Daniyal-Akhtar <167855480+Ch-Daniyal-Akhtar@users.noreply.github.com>
2025-06-12 21:19:02 +0500

    completed route.ts

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -28,6 +28,21 @@ export async function POST(request: Request) {
     const verifyCode = Math.floor(100000 + Math.random() * 90000).toString();
 
     if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: "User already exist with this email",
+          },
+          { status: 500 }
+        );
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = hashedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save();
+      }
     } else {
       const hashedPassword = await bcrypt.hash(password, 10);
       const expiryDate = new Date();
@@ -45,6 +60,30 @@ export async function POST(request: Request) {
       });
       await newUser.save();
     }
+    // send verification email
+
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "User registered successfully. Please verify your email",
+      },
+      { status: 201 }
+    );
   } catch (error) {
     console.error("Error registering user", error);
     return Response.json(
