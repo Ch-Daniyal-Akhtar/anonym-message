eb1bf7c8f93c72adcddda39c6f8046b9eacb1cef
Ch-Daniyal-Akhtar <167855480+Ch-Daniyal-Akhtar@users.noreply.github.com>
2025-06-12 12:35:10 +0500

    schemas done

--- src/schemas/verifySchema.ts
+++ src/schemas/verifySchema.ts
@@ -1,15 +1,5 @@
 import { z } from "zod";
 
-export const usernameValidation = z
-  .string()
-  .min(2, "Username must be atleast 2 characters")
-  .max(20, "Username must be no more than 20  characters")
-  .regex(/^[a-zA-Z0-9]+$/, "Username must not contain special character");
-
-export const signUpSchema = z.object({
-  ussername: usernameValidation,
-  email: z.string().email({ message: "Invalid email address" }),
-  password: z
-    .string()
-    .min(6, { message: "Password must be at least 6 characters" }),
+export const verifySchema = z.object({
+  code: z.string().length(6, "Verfication code must be six digits"),
 });
